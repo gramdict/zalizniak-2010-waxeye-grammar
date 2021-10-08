31a5158c5f0628796a75abfb7fa47f46791dce68
Sergey Slepov <slepov@gmail.com>
2021-10-08 21:22:36 +0500

    Run Converter on A.txt

--- Converter/src/ru/morpher/Main.java
+++ Converter/src/ru/morpher/Main.java
@@ -1,9 +1,38 @@
 package ru.morpher;
 
+import org.waxeye.ast.print.ArrowPrinter;
+import org.waxeye.ast.print.SexprPrinter;
+import org.waxeye.parser.ParseResult;
+
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    ZalParser parser = new ZalParser();
-	    System.out.println(parser.parse("мама жо 1a"));
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(System.getProperty("user.dir"));
+        BufferedWriter jsonFile = new BufferedWriter(new FileWriter("out/zal.json"));
+        BufferedWriter parseErrorsFile = new BufferedWriter(new FileWriter("out/parse-errors.txt"));
+        ZalParser parser = new ZalParser();
+        BufferedReader br = new BufferedReader(new FileReader("../../zalizniak-2010/dictionary/Нарицательные/А.txt"));
+        {
+            String line;
+            while ((line = br.readLine()) != null) {
+                ParseResult<ZalType> result = parser.parse(line);
+                String o;
+                if (result.getError() == null) {
+                    jsonFile.write(new SexprPrinter(result.getAST()).toString());
+                    jsonFile.newLine();
+                }
+                else {
+                    parseErrorsFile.write(line);
+                    parseErrorsFile.write(": ");
+                    parseErrorsFile.write(result.getError().toString());
+                    parseErrorsFile.newLine();
+                }
+            }
+        }
+        br.close();
+        jsonFile.close();
     }
 }
