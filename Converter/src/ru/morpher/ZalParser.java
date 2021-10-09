156eb816a9bb52d3e87c10f46afe1977bb7e3651
Sergey Slepov <slepov@gmail.com>
2021-10-09 17:15:46 +0500

    (_поговорка_) 128 of 11707 (1.09%) errors

--- Converter/src/ru/morpher/ZalParser.java
+++ Converter/src/ru/morpher/ZalParser.java
@@ -483,12 +483,12 @@ public final class ZalParser extends org.waxeye.parser.Parser<ZalType>
         edges = new ArrayList<Edge<ZalType>>();
         edges.add(new Edge<ZalType>(new AutomatonTransition<ZalType>(43), 1, false));
         edges.add(new Edge<ZalType>(new AutomatonTransition<ZalType>(44), 1, false));
-        edges.add(new Edge<ZalType>(new CharTransition<ZalType>(new char[]{' '}, new char[]{'(', ','}, new char[]{')', '-'}), 1, false));
+        edges.add(new Edge<ZalType>(new CharTransition<ZalType>(new char[]{' ', '_'}, new char[]{'(', ','}, new char[]{')', '-'}), 1, false));
         states.add(new State<ZalType>(edges, false));
         edges = new ArrayList<Edge<ZalType>>();
         edges.add(new Edge<ZalType>(new AutomatonTransition<ZalType>(43), 1, false));
         edges.add(new Edge<ZalType>(new AutomatonTransition<ZalType>(44), 1, false));
-        edges.add(new Edge<ZalType>(new CharTransition<ZalType>(new char[]{' '}, new char[]{'(', ','}, new char[]{')', '-'}), 1, false));
+        edges.add(new Edge<ZalType>(new CharTransition<ZalType>(new char[]{' ', '_'}, new char[]{'(', ','}, new char[]{')', '-'}), 1, false));
         states.add(new State<ZalType>(edges, true));
         automata.add(new FA<ZalType>(ZalType.Idiom, FA.PRUNE, states));
     }
