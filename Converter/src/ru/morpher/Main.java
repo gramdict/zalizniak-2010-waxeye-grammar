2479e1a2037ebe9873c2c067904127d44dfbb266
Sergey Slepov <slepov@gmail.com>
2021-10-10 00:31:18 +0500

    Parse all dictionary files 40062 of 109866 (36.46%) errors

--- Converter/src/ru/morpher/Main.java
+++ Converter/src/ru/morpher/Main.java
@@ -5,38 +5,49 @@ import org.waxeye.ast.print.SexprPrinter;
 import org.waxeye.parser.ParseResult;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println(System.getProperty("user.dir"));
+        ArrayList<String> files = new ArrayList<>();
+        Files.walk(Paths.get("../../zalizniak-2010/dictionary/"))
+                .filter(Files::isRegularFile)
+                .forEach(file -> files.add(file.toString()));
+
         BufferedWriter jsonFile = new BufferedWriter(new FileWriter("out/zal.json"));
         BufferedWriter parseErrorsFile = new BufferedWriter(new FileWriter("out/parse-errors.txt"));
         ZalParser parser = new ZalParser();
         int ok = 0;
         int errors = 0;
-        BufferedReader br = new BufferedReader(new FileReader("../../zalizniak-2010/dictionary/Нарицательные/А.txt"));
+        for (String fileName : files)
         {
-            String line;
-            while ((line = br.readLine()) != null) {
-                ParseResult<ZalType> result = parser.parse(line);
-                String o;
-                if (result.getError() == null) {
-                    jsonFile.write(new SexprPrinter(result.getAST()).toString());
-                    jsonFile.newLine();
-                    ++ok;
-                }
-                else {
-                    parseErrorsFile.write(line);
-                    parseErrorsFile.write(": ");
-                    parseErrorsFile.write(result.getError().toString());
-                    parseErrorsFile.newLine();
-                    ++errors;
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    ParseResult<ZalType> result = parser.parse(line);
+                    String o;
+                    if (result.getError() == null) {
+                        jsonFile.write(new SexprPrinter(result.getAST()).toString());
+                        jsonFile.newLine();
+                        ++ok;
+                    }
+                    else {
+                        parseErrorsFile.write(line);
+                        parseErrorsFile.write(": ");
+                        parseErrorsFile.write(result.getError().toString());
+                        parseErrorsFile.newLine();
+                        ++errors;
+                    }
                 }
             }
+            br.close();
         }
-        br.close();
         jsonFile.close();
         parseErrorsFile.close();
 
