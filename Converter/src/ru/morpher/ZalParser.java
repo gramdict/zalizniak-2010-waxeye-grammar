eb06b218cd4e0b57df869c910734f5e922cf3a18
Sergey Slepov <slepov@gmail.com>
2021-10-11 22:22:54 +0500

    ◑6//1 2846 of 109866 (2.59%) errors

--- Converter/src/ru/morpher/ZalParser.java
+++ Converter/src/ru/morpher/ZalParser.java
@@ -650,6 +650,18 @@ public final class ZalParser extends org.waxeye.parser.Parser<ZalType>
         edges = new ArrayList<Edge<ZalType>>();
         states.add(new State<ZalType>(edges, true));
         edges = new ArrayList<Edge<ZalType>>();
+        edges.add(new Edge<ZalType>(new CharTransition<ZalType>(new char[]{'/'}, new char[]{}, new char[]{}), 5, true));
+        edges.add(new Edge<ZalType>(new AutomatonTransition<ZalType>(43), 7, false));
+        edges.add(new Edge<ZalType>(new AutomatonTransition<ZalType>(13), 3, false));
+        states.add(new State<ZalType>(edges, true));
+        edges = new ArrayList<Edge<ZalType>>();
+        edges.add(new Edge<ZalType>(new CharTransition<ZalType>(new char[]{'/'}, new char[]{}, new char[]{}), 6, true));
+        states.add(new State<ZalType>(edges, false));
+        edges = new ArrayList<Edge<ZalType>>();
+        edges.add(new Edge<ZalType>(new AutomatonTransition<ZalType>(7), 4, false));
+        edges.add(new Edge<ZalType>(new AutomatonTransition<ZalType>(12), 4, false));
+        states.add(new State<ZalType>(edges, false));
+        edges = new ArrayList<Edge<ZalType>>();
         edges.add(new Edge<ZalType>(new AutomatonTransition<ZalType>(13), 3, false));
         states.add(new State<ZalType>(edges, true));
         automata.add(new FA<ZalType>(ZalType.Opposite_aspect, FA.LEFT, states));
