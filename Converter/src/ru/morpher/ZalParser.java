d1cd14bb805a5f920aa2c1c1eb81839a39a6dc32
Sergey Slepov <slepov@gmail.com>
2021-10-18 10:24:48 +0500

    ws after letter_seq 1224 of 109866 (1.11%) errors

--- Converter/src/ru/morpher/ZalParser.java
+++ Converter/src/ru/morpher/ZalParser.java
@@ -3917,6 +3917,9 @@ public final class ZalParser extends org.waxeye.parser.Parser<ZalType>
         edges.add(new Edge<ZalType>(new CharTransition<ZalType>(new char[]{')'}, new char[]{}, new char[]{}), 6, true));
         states.add(new State<ZalType>(edges, false));
         edges = new ArrayList<Edge<ZalType>>();
+        edges.add(new Edge<ZalType>(new AutomatonTransition<ZalType>(95), 7, false));
+        states.add(new State<ZalType>(edges, true));
+        edges = new ArrayList<Edge<ZalType>>();
         states.add(new State<ZalType>(edges, true));
         automata.add(new FA<ZalType>(ZalType.Letter_seq, FA.LEFT, states));
     }
